ff6f52561ac41dcee0b20f2c9b0fc88e1b6b85db
hsm0711 <bingo8@nate.com>
2024-04-30 14:48:10 +0900

    PLAYG-49 식당메뉴 CRUD API

---
+++ src/main/java/com/playground/api/restaurant/repository/RstrntMenuRepository.java
@@ -0,0 +1,13 @@
+package com.playground.api.restaurant.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.playground.api.restaurant.entity.RstrntMenuEntity;
+import com.playground.api.restaurant.entity.RstrntMenuPK;
+import com.playground.api.restaurant.repository.dsl.RstrntMenuRepositoryCustom;
+
+@Repository
+public interface RstrntMenuRepository extends JpaRepository<RstrntMenuEntity, RstrntMenuPK>, RstrntMenuRepositoryCustom {
+  Optional<RstrntMenuEntity> findById(RstrntMenuPK entityPk);
+}

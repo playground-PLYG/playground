12a9489b0a28161e96aaed9b8679bdb5ec8d5cd0
kimsoohwan <ubsno4012@gmail.com>
2024-05-16 09:21:38 +0900

    PLAYG-52 comment CRUD UPDATE

---
+++ src/main/java/com/playground/api/comment/entity/CommentEntityPK.java
@@ -0,0 +1,30 @@
+package com.playground.api.comment.entity;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+public class CommentEntityPK implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	/** 댓글번호 */
+	private int cmntNo;
+	
+	/** 게시물 번호*/
+	private int nttNo;
+	
+	/** 게시판 ID */
+	private String bbsId;
+}

bb39f1ed02152726adfec181a2f0d00870a5794a
parkjunwon <junwon9978@naver.com>
2024-06-07 09:13:30 +0900

    PLAYG-103-당일투표생성API

--- src/main/java/com/playground/api/vote/service/VoteService.java
+++ src/main/java/com/playground/api/vote/service/VoteService.java
@@ -14,6 +14,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
+import com.playground.api.restaurant.model.RstrntSrchRequest;
+import com.playground.api.restaurant.model.RstrntSrchResponse;
+import com.playground.api.restaurant.service.RstrntService;
 import com.playground.api.vote.entity.QestnEntity;
 import com.playground.api.vote.entity.VoteEntity;
 import com.playground.api.vote.entity.VoteIemEntity;
@@ -36,6 +39,7 @@ public class VoteService {
   private final QestnRepository qestnRepository;
   private final VoteIemRepository voteIemRepository;
   private final ModelMapper modelMapper;
+  private final RstrntService rstrntService;
 
   @Transactional(readOnly = true)
   public Page<VoteResponse> getVotePageList(VoteRequest reqData, Pageable pageable) {
@@ -305,5 +309,50 @@ public class VoteService {
       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
       LocalDateTime dateTime = LocalDateTime.parse(strDate, formatter);
       return dateTime;
+  }
+  
+  @Transactional
+  public void addTodayLunchVote() {
+    LocalDateTime now = LocalDateTime.now();
+    
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    String formatedNow = now.format(formatter);
+    
+    //1. tb_vote 테이블 등록
+    VoteEntity resVote = voteRepository.save(VoteEntity.builder()
+        .voteKndCode("LUN")
+        .voteSj(formatedNow + " 점심 투표")
+        .annymtyVoteAt("N")
+        .voteBeginDt(now)
+        .voteEndDt(now.plusDays(1))
+        .voteDeleteAt("N")
+        .build());
+    
+    
+    //2. tb_qestn 테이블 등록
+    QestnEntity resQestn = qestnRepository.save(QestnEntity.builder()
+        .voteSn(resVote.getVoteSn())
+        .qestnCn("식당 투표 목록")
+        .compnoChoiseAt("N")
+        .build());
+    
+    //3. tb_vote_iem 테이블 등록
+    List<VoteIemEntity> saveIemList = new ArrayList<>(); //한번에 insert 하기 위한 List
+    
+    RstrntSrchRequest req = new RstrntSrchRequest();
+    List<RstrntSrchResponse> resRstrntList = rstrntService.getRstrntList(req); // 식당 메뉴 조회
+    
+    for(RstrntSrchResponse res : resRstrntList) { // 조회한 식당리스트를 등록하기 위해 변환
+      saveIemList.add(
+      VoteIemEntity.builder()
+      .voteSn(resVote.getVoteSn())
+      .qestnSn(resQestn.getQestnSn())
+      .iemId(String.valueOf(res.getRestaurantSerialNo())) // 이거 DB 변경해야할듯
+      .iemNm(res.getRestaurantName())
+      .build());
+    }
+    
+    List<VoteIemEntity> resVoteIem = voteIemRepository.saveAll(saveIemList);
+    
   }
 }

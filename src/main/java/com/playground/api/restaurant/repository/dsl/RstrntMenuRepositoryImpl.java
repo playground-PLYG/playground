113a4594a0af0d6d524d38d081aeeda2d21cf75b
hsm0711 <bingo8@nate.com>
2024-06-13 15:03:48 +0900

    PLAYG-86-식당상세조회api수정 - 해시태그 없을 때 empty list 처리

--- src/main/java/com/playground/api/restaurant/repository/dsl/RstrntMenuRepositoryImpl.java
+++ src/main/java/com/playground/api/restaurant/repository/dsl/RstrntMenuRepositoryImpl.java
@@ -4,10 +4,10 @@ import java.math.BigDecimal;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import com.playground.api.hashtag.entity.QHashtagEntity;
-import com.playground.api.hashtag.model.HashtagResponse;
 import com.playground.api.restaurant.entity.QRstrntMenuEntity;
 import com.playground.api.restaurant.entity.QRstrntMenuHashtagMapngEntity;
 import com.playground.api.restaurant.entity.RstrntMenuEntity;
+import com.playground.api.restaurant.model.RstrntMenuHashtagResponse;
 import com.playground.api.restaurant.model.RstrntMenuResponse;
 import com.querydsl.core.group.GroupBy;
 import com.querydsl.core.types.Projections;
@@ -24,16 +24,31 @@ public class RstrntMenuRepositoryImpl implements RstrntMenuRepositoryCustom {
 
   @Override
   public List<RstrntMenuResponse> findAll(RstrntMenuEntity entity) {
-    return queryFactory.select(tbRstrntMenu).from(tbRstrntMenu).leftJoin(rstrntMenuHashtagMapngEntity)
+    List<RstrntMenuResponse> resultList = queryFactory.select(tbRstrntMenu).from(tbRstrntMenu).leftJoin(rstrntMenuHashtagMapngEntity)
         .on(tbRstrntMenu.rstrntSn.eq(rstrntMenuHashtagMapngEntity.rstrntSn), tbRstrntMenu.rstrntMenuSn.eq(rstrntMenuHashtagMapngEntity.rstrntMenuSn))
-        .leftJoin(hashtagEntity).on(rstrntMenuHashtagMapngEntity.hashtagSn.eq(hashtagEntity.hashtagSn))
-        .where(tbRstrntMenu.rstrntSn.eq(entity.getRstrntSn()), rstrntMenuNmLike(entity.getRstrntMenuNm()), rstrntMenuPcEq(entity.getRstrntMenuPc()))
-        .transform(GroupBy.groupBy(tbRstrntMenu.rstrntSn, tbRstrntMenu.rstrntMenuSn).list(Projections.fields(RstrntMenuResponse.class,
-            tbRstrntMenu.rstrntSn.as("restaurantSerialNo"), tbRstrntMenu.rstrntMenuSn.as("restaurantMenuSerialNo"),
-            tbRstrntMenu.rstrntMenuNm.as("menuName"), tbRstrntMenu.rstrntMenuPc.as("menuPrice"),
-            GroupBy.list(
-                Projections.fields(HashtagResponse.class, hashtagEntity.hashtagSn.as("hashtagSerialNo"), hashtagEntity.hashtagNm.as("hashtagName")))
-                .as("menuHashtagList"))));
+        .leftJoin(hashtagEntity).on(
+            rstrntMenuHashtagMapngEntity.hashtagSn.eq(hashtagEntity.hashtagSn))
+        .where(tbRstrntMenu.rstrntSn.eq(entity.getRstrntSn()), rstrntMenuNmLike(entity
+            .getRstrntMenuNm()), rstrntMenuPcEq(
+                entity.getRstrntMenuPc()))
+        .transform(GroupBy.groupBy(tbRstrntMenu.rstrntSn, tbRstrntMenu.rstrntMenuSn)
+            .list(Projections.fields(RstrntMenuResponse.class, tbRstrntMenu.rstrntSn.as("restaurantSerialNo"),
+                tbRstrntMenu.rstrntMenuSn.as("restaurantMenuSerialNo"), tbRstrntMenu.rstrntMenuNm.as("menuName"),
+                tbRstrntMenu.rstrntMenuPc.as("menuPrice"), GroupBy.list(Projections.fields(RstrntMenuHashtagResponse.class,
+                    hashtagEntity.hashtagSn.as("hashtagSerialNo"), hashtagEntity.hashtagNm.as("hashtagName"))).as("menuHashtagList"))));
+
+    return resultList.stream().map(menuResponse -> {
+      if (menuResponse.getMenuHashtagList().stream().anyMatch(hashtag -> hashtag.getHashtagSerialNo() == null)) {
+        menuResponse.setMenuHashtagList(List.of());
+      } else {
+        menuResponse.getMenuHashtagList().stream().forEach(hashtag -> {
+          hashtag.setRestaurantSerialNo(menuResponse.getRestaurantSerialNo());
+          hashtag.setRestaurantMenuSerialNo(menuResponse.getRestaurantMenuSerialNo());
+        });
+      }
+
+      return menuResponse;
+    }).toList();
   }
 
   private BooleanExpression rstrntMenuNmLike(String rstrntMenuNm) {

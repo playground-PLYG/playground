5ea72398abfd15eddb828f34dd2b6591a46cc446
hsm0711 <bingo8@nate.com>
2024-07-12 16:01:35 +0900

    SQLExpressions 사용을 위한 dependency 추가

--- src/main/java/com/playground/config/QueryDslConfig.java
+++ src/main/java/com/playground/config/QueryDslConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.sql.PostgreSQLTemplates;
+import com.querydsl.sql.SQLTemplates;
+import com.querydsl.sql.spring.SpringExceptionTranslator;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
@@ -13,8 +16,18 @@ public class QueryDslConfig {
   private EntityManager entityManager;
 
   @Bean
-  public JPAQueryFactory jpaQueryFactory() {
-    //Spring boot 3.x 버전 부터 JPQLTemplates.DEFAULT 를 넣어줘야 queryDsl에서 transForm 구문이 사용 가능
+  JPAQueryFactory jpaQueryFactory() {
+    // Spring boot 3.x 버전 부터 JPQLTemplates.DEFAULT 를 넣어줘야 queryDsl에서 transForm 구문이 사용 가능
     return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+  }
+
+  @Bean
+  com.querydsl.sql.Configuration querydslConfiguration() {
+    SQLTemplates templates = PostgreSQLTemplates.builder().build();
+    // change to your Templates
+    com.querydsl.sql.Configuration configuration = new com.querydsl.sql.Configuration(templates);
+    SpringExceptionTranslator springExceptionTranslator = new SpringExceptionTranslator();
+    configuration.setExceptionTranslator(springExceptionTranslator);
+    return configuration;
   }
 }

f5eb686bc5056c9b8a28e155c5ba6c7a339d49c7
hsm0711 <bingo8@nate.com>
2024-08-20 13:07:59 +0900

    test code 제거

--- src/main/java/com/playground/api/sample/controller/XssFilterTestController.java
+++ src/main/java/com/playground/api/sample/controller/XssFilterTestController.java
@@ -1,10 +1,8 @@
 package com.playground.api.sample.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.playground.api.sample.model.WebSocketDto;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +14,5 @@ public class XssFilterTestController {
   @PostMapping("/request-body")
   public WebSocketDto requestBodyTest(@RequestBody WebSocketDto webSocketDto) {
     return webSocketDto;
-  }
-
-  @GetMapping("/get")
-  public String getTest(@RequestParam String param) {
-    return param;
   }
 }
